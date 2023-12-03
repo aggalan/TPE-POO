9412dbeb58082461c66bdb1b0f7fb95982ed2929
Agus <aggalan@itba.edu.ar>
2023-12-03 18:50:24 -0300

    Circle changes

--- backend/model/Circle.java
+++ backend/model/Circle.java
@@ -1,26 +1,20 @@
 package backend.model;
 
-public class Circle extends Elipse implements Figure {
-
-    protected final Point centerPoint;
-    protected final double radius;
-
+public class Circle extends Ellipse {
     public Circle(Point centerPoint, double radius) {
-        this.centerPoint = centerPoint;
-        this.radius = radius;
+        super(centerPoint, radius, radius);
     }
-
     @Override
     public String toString() {
-        return String.format("Círculo [Centro: %s, Radio: %.2f]", centerPoint, radius);
+        return String.format("Círculo [Centro: %s, Radio: %.2f]", centerPoint, super.getMayorAxis());
     }
 
     public Point getCenterPoint() {
-        return centerPoint;
+        return super.getCenterPoint();
     }
 
     public double getRadius() {
-        return radius;
+        return super.getMayorAxis();
     }
 
 }

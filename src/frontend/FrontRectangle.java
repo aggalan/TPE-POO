6ca9ff351eb0856da1051fd215acc6eabec5a1f4
aggalan <127793974+aggalan@users.noreply.github.com>
2023-12-05 19:36:11 -0300

    Update FrontRectangle.java

--- src/frontend/FrontRectangle.java
+++ src/frontend/FrontRectangle.java
@@ -16,11 +16,11 @@ public class FrontRectangle<T extends Rectangle>  extends FrontFigure<T> {
     public void create(double offset) {
         Rectangle rectangle = super.getFigure();
          if(gradientStatus && offset == 0){
-            RadialGradient radialGradient = new RadialGradient(0, 0, 0.5, 0.5, 0.5, true,
+            LinearGradient linearGradient = new LinearGradient(0, 0, 1, 0, true,
                     CycleMethod.NO_CYCLE,
                     new Stop(0, getColor()),
                     new Stop(1, getColor().invert()));
-            getGc().setFill(radialGradient);
+            getGc().setFill(linearGradient);
         }
         getGc().fillRect(rectangle.getTopLeftX() + offset, rectangle.getTopLeftY() + offset,
                 rectangle.getWidth(), rectangle.getHeight());

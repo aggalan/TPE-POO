894a753baec68307e75e1193a48b8d3e60a68d14
aggalan <127793974+aggalan@users.noreply.github.com>
2023-12-05 19:35:22 -0300

    add gradient functionality

--- src/frontend/FrontRectangle.java
+++ src/frontend/FrontRectangle.java
@@ -3,7 +3,7 @@ package frontend;
 import backend.model.Rectangle;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
+import javafx.scene.paint.*;
 import javafx.scene.transform.Rotate;
 
 public class FrontRectangle<T extends Rectangle>  extends FrontFigure<T> {
@@ -15,6 +15,13 @@ public class FrontRectangle<T extends Rectangle>  extends FrontFigure<T> {
 
     public void create(double offset) {
         Rectangle rectangle = super.getFigure();
+         if(gradientStatus && offset == 0){
+            RadialGradient radialGradient = new RadialGradient(0, 0, 0.5, 0.5, 0.5, true,
+                    CycleMethod.NO_CYCLE,
+                    new Stop(0, getColor()),
+                    new Stop(1, getColor().invert()));
+            getGc().setFill(radialGradient);
+        }
         getGc().fillRect(rectangle.getTopLeftX() + offset, rectangle.getTopLeftY() + offset,
                 rectangle.getWidth(), rectangle.getHeight());
         getGc().strokeRect(rectangle.getTopLeftX(), rectangle.getTopLeftY(),

cac608815084728e2a9f4d92394849fff1ec16c4
aggalan <127793974+aggalan@users.noreply.github.com>
2023-12-05 19:36:56 -0300

    add gradient oval

--- src/frontend/FrontEllipse.java
+++ src/frontend/FrontEllipse.java
@@ -3,7 +3,7 @@ package frontend;
 import backend.model.Ellipse;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
+import javafx.scene.paint.*;
 
 public class FrontEllipse<T extends Ellipse> extends FrontFigure<T>{
 
@@ -14,6 +14,13 @@ public class FrontEllipse<T extends Ellipse> extends FrontFigure<T>{
     @Override
     public void create(double offset) {
         Ellipse ellipse = super.getFigure();
+         if(gradientStatus && offset == 0){
+            RadialGradient radialGradient = new RadialGradient(0, 0, 0.5, 0.5, 0.5, true,
+                    CycleMethod.NO_CYCLE,
+                    new Stop(0, getColor()),
+                    new Stop(1, getColor().invert()));
+            getGc().setFill(radialGradient);
+        }
         super.getGc().strokeOval(ellipse.getCenterPoint().getX() - (ellipse.getWidth() / 2), ellipse.getCenterPoint().getY() - (ellipse.getHeight() / 2), ellipse.getWidth(), ellipse.getHeight());
         super.getGc().fillOval(ellipse.getCenterPoint().getX() - (ellipse.getWidth() / 2) + offset, ellipse.getCenterPoint().getY() - (ellipse.getHeight() / 2) + offset, ellipse.getWidth(), ellipse.getHeight());
     }
